b680372c2b07cd1960fa65bde3b3d7e7d9cfffe1
grzegorz103 <grzesiek555_12@outlook.com>
2020-10-02 18:49:04 +0200

    v1.10.0

---
+++ projects/ngx-chess-board/src/lib/utils/inputs/piece-icon-input.ts
@@ -0,0 +1,17 @@
+export interface PieceIconInput{
+
+    whiteKingUrl: string;
+    whiteQueenUrl: string;
+    whiteKnightUrl: string;
+    whiteRookUrl: string;
+    whitePawnUrl: string;
+    whiteBishopUrl: string;
+    
+    blackKingUrl: string;
+    blackQueenUrl: string;
+    blackKnightUrl: string;
+    blackRookUrl: string;
+    blackPawnUrl: string;
+    blackBishopUrl: string;
+
+}
