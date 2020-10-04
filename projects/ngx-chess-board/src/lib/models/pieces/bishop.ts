54f1ccc3a0bc75876f3af016bf22d9f89b96f272
Lawrence Nseir <lawrence.nseir@check24.de>
2020-10-04 15:28:32 +0200

    [add-name-to-pieces] add names to pieces

--- projects/ngx-chess-board/src/lib/models/pieces/bishop.ts
+++ projects/ngx-chess-board/src/lib/models/pieces/bishop.ts
@@ -1,3 +1,4 @@
+import { PieceConstant } from '../../utils/unicode-constants';
 import { Board } from '../board';
 import { Color } from './color';
 import { King } from './king';
@@ -5,8 +6,13 @@ import { Piece } from './piece';
 import { Point } from './point';
 
 export class Bishop extends Piece {
-    constructor(point: Point, color: Color, image: string, board: Board) {
-        super(point, color, image, 3, board);
+    constructor(
+        point: Point,
+        color: Color,
+        constant: PieceConstant,
+        board: Board
+    ) {
+        super(point, color, constant, 3, board);
     }
 
     getPossibleMoves(): Point[] {
@@ -62,7 +68,13 @@ export class Bishop extends Piece {
 
         for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j) {
             // lewa gorna przekatna
-            if (this.board.isFieldTakenByEnemy(i, j, this.color === Color.WHITE ? Color.BLACK : Color.WHITE)) {
+            if (
+                this.board.isFieldTakenByEnemy(
+                    i,
+                    j,
+                    this.color === Color.WHITE ? Color.BLACK : Color.WHITE
+                )
+            ) {
                 possiblePoints.push(new Point(i, j));
                 break;
             } else {
@@ -74,7 +86,13 @@ export class Bishop extends Piece {
 
         for (let i = row - 1, j = col + 1; i >= 0 && j < 8; --i, ++j) {
             // prawa gorna przekatna
-            if (this.board.isFieldTakenByEnemy(i, j, this.color === Color.WHITE ? Color.BLACK : Color.WHITE)) {
+            if (
+                this.board.isFieldTakenByEnemy(
+                    i,
+                    j,
+                    this.color === Color.WHITE ? Color.BLACK : Color.WHITE
+                )
+            ) {
                 possiblePoints.push(new Point(i, j));
                 break;
             } else {
@@ -86,7 +104,13 @@ export class Bishop extends Piece {
 
         for (let i = row + 1, j = col - 1; i < 8 && j >= 0; ++i, --j) {
             // lewa dolna przekatna
-            if (this.board.isFieldTakenByEnemy(i, j, this.color === Color.WHITE ? Color.BLACK : Color.WHITE)) {
+            if (
+                this.board.isFieldTakenByEnemy(
+                    i,
+                    j,
+                    this.color === Color.WHITE ? Color.BLACK : Color.WHITE
+                )
+            ) {
                 possiblePoints.push(new Point(i, j));
                 break;
             } else {
@@ -98,7 +122,13 @@ export class Bishop extends Piece {
 
         for (let i = row + 1, j = col + 1; i < 8 && j < 8; ++i, ++j) {
             // prawa dolna przekatna
-            if (this.board.isFieldTakenByEnemy(i, j, this.color === Color.WHITE ? Color.BLACK : Color.WHITE)) {
+            if (
+                this.board.isFieldTakenByEnemy(
+                    i,
+                    j,
+                    this.color === Color.WHITE ? Color.BLACK : Color.WHITE
+                )
+            ) {
                 possiblePoints.push(new Point(i, j));
                 break;
             } else {
