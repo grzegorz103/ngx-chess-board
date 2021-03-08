49995b72958519741c20d8e83306a918432405ee
Ankit <ankitshahit09@gmail.com>
2021-03-08 16:42:08 +0530

    Added free mode feature

--- projects/ngx-chess-board/src/lib/models/board.ts
+++ projects/ngx-chess-board/src/lib/models/board.ts
@@ -1,271 +1,328 @@
-import { cloneDeep } from 'lodash';
-import { Bishop } from './pieces/bishop';
-import { Color } from './pieces/color';
-import { King } from './pieces/king';
-import { Knight } from './pieces/knight';
-import { Pawn } from './pieces/pawn';
-import { Piece } from './pieces/piece';
-import { Point } from './pieces/point';
-import { Queen } from './pieces/queen';
-import { Rook } from './pieces/rook';
+import {cloneDeep} from "lodash";
+import {Bishop} from "./pieces/bishop";
+import {Color} from "./pieces/color";
+import {King} from "./pieces/king";
+import {Knight} from "./pieces/knight";
+import {Pawn} from "./pieces/pawn";
+import {Piece} from "./pieces/piece";
+import {Point} from "./pieces/point";
+import {Queen} from "./pieces/queen";
+import {Rook} from "./pieces/rook";
 
 export class Board {
-    board: number[][] = [];
-    pieces: Piece[] = [];
+	board: number[][] = [];
+	pieces: Piece[] = [];
 
-    enPassantPoint: Point = null;
-    enPassantPiece: Piece = null;
-    lastMoveSrc: Point = null;
-    lastMoveDest: Point = null;
-    activePiece: Piece;
+	enPassantPoint: Point = null;
+	enPassantPiece: Piece = null;
+	lastMoveSrc: Point = null;
+	lastMoveDest: Point = null;
+	activePiece: Piece;
 
-    blackKingChecked: boolean;
-    possibleCaptures: any[] = [];
-    possibleMoves: Point[] = [];
-    whiteKingChecked: boolean;
+	blackKingChecked: boolean;
+	possibleCaptures: any[] = [];
+	possibleMoves: Point[] = [];
+	whiteKingChecked: boolean;
 
-    currentWhitePlayer = true;
-    reverted = false;
-    fullMoveCount = 1;
-    fen: string;
+	isFreeMode = false;
+	currentWhitePlayer = true;
+	reverted = false;
+	fullMoveCount = 1;
+	fen: string;
 
-    constructor() {
-        for (let i = 0; i < 8; ++i) {
-            this.board[i] = [];
-            for (let j = 0; j < 8; ++j) {
-                this.board[i][j] = 0;
-            }
-        }
-    }
+	constructor() {
+		for (let i = 0; i < 8; ++i) {
+			this.board[i] = [];
+			for (let j = 0; j < 8; ++j) {
+				this.board[i][j] = 0;
+			}
+		}
+	}
 
-    isXYInPossibleMoves(row: number, col: number): boolean {
-        return this.possibleMoves.some((move) => move.row === row && move.col === col);
-    }
+	isXYInPossibleMoves(row: number, col: number): boolean {
+		return this.possibleMoves.some(
+			(move) => move.row === row && move.col === col
+		);
+	}
 
-    isXYInPossibleCaptures(row: number, col: number): boolean {
-        return this.possibleCaptures.some((capture) => capture.row === row && capture.col === col);
-    }
+	isXYInPossibleCaptures(row: number, col: number): boolean {
+		return this.possibleCaptures.some(
+			(capture) => capture.row === row && capture.col === col
+		);
+	}
 
-    isXYInSourceMove(i: number, j: number) {
-        return this.lastMoveSrc && this.lastMoveSrc.row === i && this.lastMoveSrc.col === j;
-    }
+	isXYInSourceMove(i: number, j: number) {
+		return (
+			this.lastMoveSrc &&
+			this.lastMoveSrc.row === i &&
+			this.lastMoveSrc.col === j
+		);
+	}
 
-    isXYInDestMove(i: number, j: number) {
-        return this.lastMoveDest && this.lastMoveDest.row === i && this.lastMoveDest.col === j;
-    }
+	isXYInDestMove(i: number, j: number) {
+		return (
+			this.lastMoveDest &&
+			this.lastMoveDest.row === i &&
+			this.lastMoveDest.col === j
+		);
+	}
 
-    isXYInActiveMove(i: number, j: number) {
-        return this.activePiece && this.activePiece.point.row === i && this.activePiece.point.col === j;
-    }
+	isXYInActiveMove(i: number, j: number) {
+		return (
+			this.activePiece &&
+			this.activePiece.point.row === i &&
+			this.activePiece.point.col === j
+		);
+	}
 
-    isPointInPossibleMoves(point: Point): boolean {
-        return this.possibleMoves.some((move) => move.row === point.row && move.col === point.col);
-    }
+	isPointInPossibleMoves(point: Point): boolean {
+		return (
+			this.isFreeModeEnabled() ||
+			this.possibleMoves.some(
+				(move) => move.row === point.row && move.col === point.col
+			)
+		);
+	}
 
-    isPointInPossibleCaptures(point: Point): boolean {
-        return this.possibleCaptures.some((capture) => capture.row === point.row && capture.col === point.col);
-    }
+	isPointInPossibleCaptures(point: Point): boolean {
+		return (
+			this.isFreeModeEnabled() ||
+			this.possibleCaptures.some(
+				(capture) => capture.row === point.row && capture.col === point.col
+			)
+		);
+	}
 
-    reset() {
-        this.lastMoveDest = null;
-        this.lastMoveSrc = null;
-        this.whiteKingChecked = false;
-        this.blackKingChecked = false;
-        this.possibleCaptures = [];
-        this.possibleMoves = [];
-        this.activePiece = null;
-        this.reverted = false;
-        this.currentWhitePlayer = true;
-        this.enPassantPoint = null;
-        this.enPassantPiece = null;
-        this.fullMoveCount = 1;
-        this.calculateFEN();
-    }
+	reset() {
+		this.lastMoveDest = null;
+		this.lastMoveSrc = null;
+		this.whiteKingChecked = false;
+		this.blackKingChecked = false;
+		this.possibleCaptures = [];
+		this.possibleMoves = [];
+		this.activePiece = null;
+		this.reverted = false;
+		this.currentWhitePlayer = true;
+		this.enPassantPoint = null;
+		this.enPassantPiece = null;
+		this.fullMoveCount = 1;
+		this.calculateFEN();
+		this.isFreeMode = false;
+	}
 
-    reverse() {
-        this.reverted = !this.reverted;
-        this.activePiece = null;
-        this.possibleMoves = [];
-        this.possibleCaptures = [];
+	reverse() {
+		this.reverted = !this.reverted;
+		this.activePiece = null;
+		this.possibleMoves = [];
+		this.possibleCaptures = [];
 
-        this.pieces.forEach((piece: Piece) => this.reversePoint(piece.point));
+		this.pieces.forEach((piece: Piece) => this.reversePoint(piece.point));
 
-        this.reversePoint(this.lastMoveSrc);
+		this.reversePoint(this.lastMoveSrc);
 
-        if (this.enPassantPoint && this.enPassantPiece) {
-            this.reversePoint(this.enPassantPoint);
-        }
-    }
+		if (this.enPassantPoint && this.enPassantPiece) {
+			this.reversePoint(this.enPassantPoint);
+		}
+	}
 
-    clone(): Board {
-        return cloneDeep(this);
-    }
+	clone(): Board {
+		return cloneDeep(this);
+	}
 
-    isFieldTakenByEnemy(row: number, col: number, enemyColor: Color): boolean {
-        if (row > 7 || row < 0 || col > 7 || col < 0) {
-            return false;
-        }
-        return this.pieces.some(
-            (piece) => piece.point.col === col && piece.point.row === row && piece.color === enemyColor
-        );
-    }
+	isFieldTakenByEnemy(row: number, col: number, enemyColor: Color): boolean {
+		if (row > 7 || row < 0 || col > 7 || col < 0) {
+			return false;
+		}
+		return this.pieces.some(
+			(piece) =>
+				piece.point.col === col &&
+				piece.point.row === row &&
+				piece.color === enemyColor
+		);
+	}
 
-    isFieldEmpty(row: number, col: number): boolean {
-        if (row > 7 || row < 0 || col > 7 || col < 0) {
-            return false;
-        }
-        return !this.pieces.some((piece) => piece.point.col === col && piece.point.row === row);
-    }
+	isFieldEmpty(row: number, col: number): boolean {
+		if (row > 7 || row < 0 || col > 7 || col < 0) {
+			return false;
+		}
+		return !this.pieces.some(
+			(piece) => piece.point.col === col && piece.point.row === row
+		);
+	}
 
-    isFieldUnderAttack(row: number, col: number, color: Color) {
-        return this.pieces
-            .filter((piece) => piece.color === color)
-            .some((piece) => piece.getCoveredFields().some((field) => field.col === col && field.row === row));
-    }
+	isFieldUnderAttack(row: number, col: number, color: Color) {
+		return this.pieces
+			.filter((piece) => piece.color === color)
+			.some((piece) =>
+				piece
+					.getCoveredFields()
+					.some((field) => field.col === col && field.row === row)
+			);
+	}
 
-    getPieceByField(row: number, col: number): Piece {
-        if (this.isFieldEmpty(row, col)) {
-            //   throw new Error('Piece not found');
-            return undefined;
-        }
+	getPieceByField(row: number, col: number): Piece {
+		if (this.isFieldEmpty(row, col)) {
+			//   throw new Error('Piece not found');
+			return undefined;
+		}
 
-        return this.pieces.find((piece) => piece.point.col === col && piece.point.row === row);
-    }
+		return this.pieces.find(
+			(piece) => piece.point.col === col && piece.point.row === row
+		);
+	}
 
-    isKingInCheck(color: Color, pieces: Piece[]): boolean {
-        const king = pieces.find((piece) => piece.color === color && piece instanceof King);
+	isKingInCheck(color: Color, pieces: Piece[]): boolean {
+		const king = pieces.find(
+			(piece) => piece.color === color && piece instanceof King
+		);
 
-        if (king) {
-            return pieces.some(
-                (piece) =>
-                    piece
-                        .getPossibleCaptures()
-                        .some((point) => point.col === king.point.col && point.row === king.point.row) &&
-                    piece.color !== color
-            );
-        }
-        return false;
-    }
+		if (king) {
+			return pieces.some(
+				(piece) =>
+					piece
+						.getPossibleCaptures()
+						.some(
+							(point) =>
+								point.col === king.point.col && point.row === king.point.row
+						) && piece.color !== color
+			);
+		}
+		return false;
+	}
 
-    getKingByColor(color: Color): King {
-        return this.pieces.find((piece) => piece instanceof King && piece.color === color) as King;
-    }
+	getKingByColor(color: Color): King {
+		return this.pieces.find(
+			(piece) => piece instanceof King && piece.color === color
+		) as King;
+	}
 
-    getCastleFENString(color: Color) {
-        const king = this.getKingByColor(color);
+	getCastleFENString(color: Color) {
+		const king = this.getKingByColor(color);
 
-        if (king.isMovedAlready) {
-            return '';
-        }
+		if (king.isMovedAlready) {
+			return "";
+		}
 
-        let fen = '';
-        const leftRook = this.getPieceByField(king.point.row, 0);
-        const rightRook = this.getPieceByField(king.point.row, 7);
+		let fen = "";
+		const leftRook = this.getPieceByField(king.point.row, 0);
+		const rightRook = this.getPieceByField(king.point.row, 7);
 
-        if (rightRook instanceof Rook && rightRook.color === color) {
-            if (!rightRook.isMovedAlready) {
-                fen += this.reverted ? 'q' : 'k';
-            }
-        }
+		if (rightRook instanceof Rook && rightRook.color === color) {
+			if (!rightRook.isMovedAlready) {
+				fen += this.reverted ? "q" : "k";
+			}
+		}
 
-        if (leftRook instanceof Rook && leftRook.color === color) {
-            if (!leftRook.isMovedAlready) {
-                fen += this.reverted ? 'k' : 'q';
-            }
-        }
+		if (leftRook instanceof Rook && leftRook.color === color) {
+			if (!leftRook.isMovedAlready) {
+				fen += this.reverted ? "k" : "q";
+			}
+		}
 
-        fen = fen.split('').sort().join('');
-        return color === Color.BLACK ? fen : fen.toUpperCase();
-    }
+		fen = fen.split("").sort().join("");
+		return color === Color.BLACK ? fen : fen.toUpperCase();
+	}
 
-    getEnPassantFENString() {
-        if (this.enPassantPoint) {
-            if (this.reverted) {
-                return String.fromCharCode(104 - this.enPassantPoint.col) + (this.enPassantPoint.row + 1);
-            } else {
-                return String.fromCharCode(97 + this.enPassantPoint.col) + (Math.abs(this.enPassantPoint.row - 7) + 1);
-            }
-        } else {
-            return '-';
-        }
-    }
+	getEnPassantFENString() {
+		if (this.enPassantPoint) {
+			if (this.reverted) {
+				return (
+					String.fromCharCode(104 - this.enPassantPoint.col) +
+					(this.enPassantPoint.row + 1)
+				);
+			} else {
+				return (
+					String.fromCharCode(97 + this.enPassantPoint.col) +
+					(Math.abs(this.enPassantPoint.row - 7) + 1)
+				);
+			}
+		} else {
+			return "-";
+		}
+	}
 
-    calculateFEN() {
-        let fen = '';
-        for (let i = 0; i < 8; ++i) {
-            let emptyFields = 0;
-            for (let j = 0; j < 8; ++j) {
-                const foundPiece = this.pieces.find((piece) => piece.point.col === j && piece.point.row === i);
-                if (foundPiece) {
-                    if (emptyFields > 0) {
-                        fen += emptyFields;
-                        emptyFields = 0;
-                    }
+	isFreeModeEnabled() {
+		return this.isFreeMode;
+	}
 
-                    if (foundPiece instanceof Rook) {
-                        fen += foundPiece.color === Color.BLACK ? 'r' : 'R';
-                    } else {
-                        if (foundPiece instanceof Knight) {
-                            fen += foundPiece.color === Color.BLACK ? 'n' : 'N';
-                        } else {
-                            if (foundPiece instanceof Bishop) {
-                                fen += foundPiece.color === Color.BLACK ? 'b' : 'B';
-                            } else {
-                                if (foundPiece instanceof Queen) {
-                                    fen += foundPiece.color === Color.BLACK ? 'q' : 'Q';
-                                } else {
-                                    if (foundPiece instanceof King) {
-                                        fen += foundPiece.color === Color.BLACK ? 'k' : 'K';
-                                    } else {
-                                        if (foundPiece instanceof Pawn) {
-                                            fen += foundPiece.color === Color.BLACK ? 'p' : 'P';
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                } else {
-                    ++emptyFields;
-                }
-            }
+	calculateFEN() {
+		let fen = "";
+		for (let i = 0; i < 8; ++i) {
+			let emptyFields = 0;
+			for (let j = 0; j < 8; ++j) {
+				const foundPiece = this.pieces.find(
+					(piece) => piece.point.col === j && piece.point.row === i
+				);
+				if (foundPiece) {
+					if (emptyFields > 0) {
+						fen += emptyFields;
+						emptyFields = 0;
+					}
 
-            if (emptyFields > 0) {
-                fen += emptyFields;
-            }
+					if (foundPiece instanceof Rook) {
+						fen += foundPiece.color === Color.BLACK ? "r" : "R";
+					} else {
+						if (foundPiece instanceof Knight) {
+							fen += foundPiece.color === Color.BLACK ? "n" : "N";
+						} else {
+							if (foundPiece instanceof Bishop) {
+								fen += foundPiece.color === Color.BLACK ? "b" : "B";
+							} else {
+								if (foundPiece instanceof Queen) {
+									fen += foundPiece.color === Color.BLACK ? "q" : "Q";
+								} else {
+									if (foundPiece instanceof King) {
+										fen += foundPiece.color === Color.BLACK ? "k" : "K";
+									} else {
+										if (foundPiece instanceof Pawn) {
+											fen += foundPiece.color === Color.BLACK ? "p" : "P";
+										}
+									}
+								}
+							}
+						}
+					}
+				} else {
+					++emptyFields;
+				}
+			}
 
-            fen += '/';
-        }
+			if (emptyFields > 0) {
+				fen += emptyFields;
+			}
 
-        fen = fen.substr(0, fen.length - 1);
+			fen += "/";
+		}
 
-        if (this.reverted) {
-            fen = fen.split('').reverse().join('');
-        }
+		fen = fen.substr(0, fen.length - 1);
 
-        fen += ' ' + (this.currentWhitePlayer ? 'w' : 'b');
-        const whiteEnPassant = this.getCastleFENString(Color.WHITE);
-        const blackEnPassant = this.getCastleFENString(Color.BLACK);
-        let concatedEnPassant = whiteEnPassant + blackEnPassant;
-        if (!concatedEnPassant) {
-            concatedEnPassant = '-';
-        }
+		if (this.reverted) {
+			fen = fen.split("").reverse().join("");
+		}
 
-        fen += ' ' + concatedEnPassant;
-        fen += ' ' + this.getEnPassantFENString();
-        fen += ' ' + 0;
-        fen += ' ' + this.fullMoveCount;
-        this.fen = fen;
-    }
+		fen += " " + (this.currentWhitePlayer ? "w" : "b");
+		const whiteEnPassant = this.getCastleFENString(Color.WHITE);
+		const blackEnPassant = this.getCastleFENString(Color.BLACK);
+		let concatedEnPassant = whiteEnPassant + blackEnPassant;
+		if (!concatedEnPassant) {
+			concatedEnPassant = "-";
+		}
 
-    isXYInPointSelection(i: number, j: number) {
-        return false;
-    }
+		fen += " " + concatedEnPassant;
+		fen += " " + this.getEnPassantFENString();
+		fen += " " + 0;
+		fen += " " + this.fullMoveCount;
+		this.fen = fen;
+	}
 
-    private reversePoint(point: Point) {
-        if (point) {
-            point.row = Math.abs(point.row - 7);
-            point.col = Math.abs(point.col - 7);
-        }
-    }
+	isXYInPointSelection(i: number, j: number) {
+		return false;
+	}
+
+	private reversePoint(point: Point) {
+		if (point) {
+			point.row = Math.abs(point.row - 7);
+			point.col = Math.abs(point.col - 7);
+		}
+	}
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,98 +1,101 @@
-import { Component, ViewChild } from '@angular/core';
-import { MoveChange, NgxChessBoardComponent } from 'ngx-chess-board';
-import { PieceIconInput } from 'ngx-chess-board';
-import { FenComponent } from './components/fen/fen.component';
-import { MovesComponent } from './components/moves/moves.component';
+import {Component, ViewChild} from "@angular/core";
+import {MoveChange, NgxChessBoardComponent} from "ngx-chess-board";
+import {PieceIconInput} from "ngx-chess-board";
+import {FenComponent} from "./components/fen/fen.component";
+import {MovesComponent} from "./components/moves/moves.component";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-    @ViewChild('board')
-    boardManager: NgxChessBoardComponent;
+	@ViewChild("board")
+	boardManager: NgxChessBoardComponent;
 
-    @ViewChild('fenManager') fenManager: FenComponent;
-    public fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
-    private currentStateIndex: number;
-    manualMove = 'd2d4';
-    icons: PieceIconInput = {
-        blackBishopUrl: '',
-        blackKingUrl: '',
-        blackKnightUrl: '',
-        blackPawnUrl: '',
-        blackQueenUrl: '',
-        blackRookUrl: '',
-        whiteBishopUrl: '',
-        whiteKingUrl: '',
-        whiteKnightUrl: '',
-        whitePawnUrl: '',
-        whiteQueenUrl: '',
-        whiteRookUrl: ''
-    };
+	@ViewChild("fenManager") fenManager: FenComponent;
+	public fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+	private currentStateIndex: number;
+	manualMove = "d2d4";
+	icons: PieceIconInput = {
+		blackBishopUrl: "",
+		blackKingUrl: "",
+		blackKnightUrl: "",
+		blackPawnUrl: "",
+		blackQueenUrl: "",
+		blackRookUrl: "",
+		whiteBishopUrl: "",
+		whiteKingUrl: "",
+		whiteKnightUrl: "",
+		whitePawnUrl: "",
+		whiteQueenUrl: "",
+		whiteRookUrl: "",
+	};
 
-    public darkTileColor = 'rgb(97, 84, 61)';
-    public lightTileColor = '#BAA378';
-    public size = 400;
-    public dragDisabled = false;
-    public drawDisabled = false;
-    public lightDisabled = false;
-    public darkDisabled = false;
+	public darkTileColor = "rgb(97, 84, 61)";
+	public lightTileColor = "#BAA378";
+	public size = 400;
+	public dragDisabled = false;
+	public drawDisabled = false;
+	public lightDisabled = false;
+	public darkDisabled = false;
+	freeMode = false;
+	public reset(): void {
+		alert("Resetting board");
+		this.boardManager.reset();
+		this.fen = this.boardManager.getFEN();
+	}
 
-    public reset(): void {
-        alert('Resetting board');
-        this.boardManager.reset();
-        this.fen = this.boardManager.getFEN();
-    }
+	public reverse(): void {
+		this.boardManager.reverse();
+	}
 
-    public reverse(): void {
-        this.boardManager.reverse();
-    }
+	public undo(): void {
+		this.boardManager.undo();
+		this.fen = this.boardManager.getFEN();
+	}
 
-    public undo(): void {
-        this.boardManager.undo();
-        this.fen = this.boardManager.getFEN();
-    }
+	public setFen(): void {
+		if (this.fen) {
+			this.boardManager.setFEN(this.fen);
+		}
+	}
 
-    public setFen(): void {
-        if (this.fen) {
-            this.boardManager.setFEN(this.fen);
-        }
-    }
+	public moveCallback(move: MoveChange): void {
+		this.fen = this.boardManager.getFEN();
+		console.log(move);
+	}
 
-    public moveCallback(move: MoveChange): void {
-        this.fen = this.boardManager.getFEN();
-        console.log(move);
-    }
+	public moveManual(): void {
+		this.boardManager.move(this.manualMove);
+	}
 
-    public moveManual(): void {
-        this.boardManager.move(this.manualMove);
-    }
+	getFEN() {
+		let fen = this.boardManager.getFEN();
+		alert(fen);
+	}
 
-    getFEN() {
-        let fen = this.boardManager.getFEN();
-        alert(fen);
-    }
+	showMoveHistory() {
+		alert(JSON.stringify(this.boardManager.getMoveHistory()));
+	}
 
-    showMoveHistory() {
-        alert(JSON.stringify(this.boardManager.getMoveHistory()));
-    }
+	switchDrag() {
+		this.dragDisabled = !this.dragDisabled;
+	}
 
-    switchDrag() {
-        this.dragDisabled = !this.dragDisabled;
-    }
+	switchDraw() {
+		this.drawDisabled = !this.drawDisabled;
+	}
 
-    switchDraw() {
-        this.drawDisabled = !this.drawDisabled;
-    }
+	switchDarkDisabled() {
+		this.darkDisabled = !this.darkDisabled;
+	}
 
-    switchDarkDisabled() {
-        this.darkDisabled = !this.darkDisabled;
-    }
+	switchLightDisabled() {
+		this.lightDisabled = !this.lightDisabled;
+	}
 
-    switchLightDisabled() {
-        this.lightDisabled = !this.lightDisabled;
-    }
-
+	switchFreeMode() {
+		this.freeMode = !this.freeMode;
+	}
 }
