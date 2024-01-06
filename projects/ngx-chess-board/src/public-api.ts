8b7a177ab73e6dfe0b220eae9a9585f16e1b0f98
Lawrence Nseir <enjoy.1993@hotmail.com>
2024-01-06 19:13:08 +0100

    feat: expose model in public api

--- projects/ngx-chess-board/src/public-api.ts
+++ projects/ngx-chess-board/src/public-api.ts
@@ -10,8 +10,7 @@ export * from './lib/ngx-chess-board.component';
 export * from './lib/piece-promotion/piece-promotion-modal/piece-promotion-modal.component';
 
 export * from './lib/ngx-chess-board-view';
-export * from './lib/service/ngx-chess-board.service';
-export * from './lib/service/ngx-chess-board.service';
+export * from './lib/models/board';
 export * from './lib/engine/outputs/move-change/move-change'
 
 /*
