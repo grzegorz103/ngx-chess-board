5da45487a367efb3d992826d320b5f4f8a39bcf4
Lawrence Nseir <enjoy.1993@hotmail.com>
2024-01-14 15:12:06 +0100

    refactor: usage only of color enum

--- projects/ngx-chess-board/src/lib/piece-promotion/piece-promotion-modal/piece-promotion-modal.component.ts
+++ projects/ngx-chess-board/src/lib/piece-promotion/piece-promotion-modal/piece-promotion-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { PieceIconInput } from '../../utils/inputs/piece-icon-input';
+import { Color } from '../../models/pieces/color';
 
 @Component({
     selector: 'app-piece-promotion-modal',
@@ -14,7 +15,7 @@ export class PiecePromotionModalComponent {
     pieceIconInput: PieceIconInput;
 
     @Input()
-    color = 'white';
+    color: Color;
 
     opened = false;
     private onCloseCallback: (letter: string) => void;
@@ -41,16 +42,16 @@ export class PiecePromotionModalComponent {
         let coloredPiece = '';
         switch (piece.toLowerCase()) {
             case 'queen':
-                coloredPiece = this.color === 'white' ? this.pieceIconInput.whiteQueenUrl : this.pieceIconInput.blackQueenUrl;
+                coloredPiece = this.color === Color.WHITE ? this.pieceIconInput.whiteQueenUrl : this.pieceIconInput.blackQueenUrl;
                 break;
             case 'rook':
-                coloredPiece = this.color === 'white' ? this.pieceIconInput.whiteRookUrl : this.pieceIconInput.blackRookUrl;
+                coloredPiece = this.color === Color.WHITE ? this.pieceIconInput.whiteRookUrl : this.pieceIconInput.blackRookUrl;
                 break;
             case 'bishop':
-                coloredPiece = this.color === 'white' ? this.pieceIconInput.whiteBishopUrl : this.pieceIconInput.blackBishopUrl;
+                coloredPiece = this.color === Color.WHITE ? this.pieceIconInput.whiteBishopUrl : this.pieceIconInput.blackBishopUrl;
                 break;
             case 'knight':
-                coloredPiece = this.color === 'white' ? this.pieceIconInput.whiteKnightUrl : this.pieceIconInput.blackKnightUrl;
+                coloredPiece = this.color === Color.WHITE ? this.pieceIconInput.whiteKnightUrl : this.pieceIconInput.blackKnightUrl;
                 break;
         }
 
